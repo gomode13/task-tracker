f1a2696cc7a2429091e61ac4078b0d084233e2cf
gomode13 <lil.god.mode.13@gmail.com>
2026-08-06 19:42:05 +0300

    Add Task model and relationship with User

--- task-tracker-backend/app/users/models.py
+++ task-tracker-backend/app/users/models.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy import DateTime, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
+
+if TYPE_CHECKING:
+    from app.tasks.models import Task
 
 
 class User(Base):
@@ -13,3 +17,6 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(320), unique=True)
     password_hash: Mapped[str] = mapped_column(String(255))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user", cascade="all, delete-orphan",
+                                               passive_deletes=True)
