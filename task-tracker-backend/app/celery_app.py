5a10e7b280f16fefd66666bc4dbca41e946333eb
gomode13 <lil.god.mode.13@gmail.com>
2026-08-11 16:47:47 +0300

    Add daily report scheduler on Celery Beat

--- task-tracker-backend/app/celery_app.py
+++ task-tracker-backend/app/celery_app.py
@@ -1,6 +1,15 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from app.config import settings
 
-celery_app = Celery("task_tracker", broker=settings.rabbitmq_url, include=["app.email_sender.tasks"])
-celery_app.conf.update(task_serializer="json")
+celery_app = Celery(
+    "task_tracker", broker=settings.rabbitmq_url, include=["app.email_sender.tasks", "app.scheduler.tasks"]
+)
+BEAT_SCHEDULE = {
+    "send-daily-reports": {
+        "task": "app.scheduler.tasks.send_daily_reports",
+        "schedule": crontab(minute=0, hour=0),
+    },
+}
+celery_app.conf.update(task_serializer="json", timezone="Europe/Moscow", beat_schedule=BEAT_SCHEDULE)
