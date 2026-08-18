b289dd56526f6e47ffb2cd1a8d365b4a30e198fc
gomode13 <lil.god.mode.13@gmail.com>
2026-08-18 16:46:26 +0300

    Run Kafka consumer on service start

--- summarization-service/app/main.py
+++ summarization-service/app/main.py
@@ -1,8 +1,12 @@
+import asyncio
 import logging
 
+from app.kafka.consumer import consume_daily_report_requests
 from app.logging_config import setup_logging
 
 setup_logging()
 
 logger = logging.getLogger(__name__)
+
 logger.info("Summarization service started")
+asyncio.run(consume_daily_report_requests())
