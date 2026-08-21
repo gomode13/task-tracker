acd692417de72910eccd8aed45cc42b46c18b569
gomode13 <lil.god.mode.13@gmail.com>
2026-08-21 16:25:44 +0300

    Add database settings to the summarization service config

--- summarization-service/app/config.py
+++ summarization-service/app/config.py
@@ -10,13 +10,22 @@ class Settings(BaseSettings):
     KAFKA_HOST: str
     KAFKA_PORT: int
 
-    GIGACHAT_AUTH_KEY: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
+    GIGACHAT_AUTH_KEY: str
     GIGACHAT_CA_CERT_FILE: str
 
     @property
     def kafka_bootstrap_servers(self) -> str:
         return f"{self.KAFKA_HOST}:{self.KAFKA_PORT}"
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 settings = Settings()
